5610ff2b4be879b9b6fc3feda146c1763f648b1c
vitika9 <vitika.program@gmail.com>
2021-06-26 18:11:40 +0530

    Revert "4th commit"
    This reverts commit d0c6c147de7e054b4b47d759551781b447bd5143.

--- lib/main.dart
+++ lib/main.dart
@@ -31,7 +31,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Event Counter',
+        title: 'My app',
         theme: ThemeData(
           primarySwatch: Colors.blue,
         ),
